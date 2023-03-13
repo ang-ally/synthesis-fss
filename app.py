900565087863372dd498314bb5276a5b17e0f205
Angelyn Sweety <68515023+ang-ally@users.noreply.github.com>
2023-03-13 18:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pickle
-import pybase64
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import RobustScaler
@@ -9,26 +8,6 @@ model_d = pickle.load(open('d_prediction.sav','rb'))
 model_s = pickle.load(open('s_prediction.sav','rb'))
 
 st.title("Synthesis of Design Parameters of a Square Loop based FSS using ML Approach/Technique")
-
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return pybase64.b64encode(data).decode()
-
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-color:'pink';
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('./bg2.png')
 
 st.header('For FR-4 Substrate')
 
